de64b399ade17bad99244a57fa8ff67cf2175344
ayyappanveera <53147833+ayyappanveera@users.noreply.github.com>
2019-08-02 15:49:33 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,7 +1,7 @@
 n=int(input())
-o=list(map(int,input().split()))[0:n]
-o.sort(reverse=True)
+v=list(map(int,input().split()))[0:n]
+v.sort(reverse=True)
 i=0
 while i<n:
-    print(o[i],end="\n")
+    print(v[i],end="\n")
     i+=1
